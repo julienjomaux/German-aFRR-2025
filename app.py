b0b872f1b1ae24f8a7a9530fdfef850436951ed7
Julien Jomaux <julien.jomaux@gmail.com>
2026-01-25 21:01:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 from datetime import date
 import os
 import glob
+from typing import Optional, List, Dict
 
 st.set_page_config(layout="wide")
 st.title("aFRR Capacity Prices in Germany (2021–2025)")
@@ -414,6 +415,7 @@ else:
         st.plotly_chart(fig, use_container_width=True)
     else:
         st.info("No data available for the selected date.")
+
 
 
 
