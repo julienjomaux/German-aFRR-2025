35851e072ba3c678fd583b84c44e051037bdd2d8
Julien Jomaux <julien.jomaux@gmail.com>
2026-02-09 15:37:39 +0100

    Update app aFRR free.py

--- app aFRR free.py
+++ app aFRR free.py
@@ -25,11 +25,23 @@ capacity prices in Germany for the years 2021–2025.
 
 **Data source:** Regelleistung.net
 
-**More insights:** [GEM Energy Analytics](https://gemenergyanalytics.substack.com/)  
+**More insights:** GEM Energy Analytics  
 **Connect with me:** Julien Jomaux  
 **Email me:** julien.jomaux@gmail.com
+"""
+)
 
-If you want to support, please consider becoming a paying member of [GEM Energy Analytics](https://gemenergyanalytics.substack.com/). 
+st.markdown(
+    """
+    If you want to access all the apps of GEM Energy Analytics, please sign up following the link below. 
+
+    Currently, the fee is 30 € per month. When the payment is done, you will receive a password 
+    that will grant you access to all apps. Every month, you will receive an email with a new password 
+    to access the apps (except if you unsubscribe). 
+    Feel free to reach out at Julien.jomaux@gmail.com
+
+    Sign Up Now :metal:
+    """
 )
 
 # -------------------------------------------------------------------------
@@ -356,5 +368,4 @@ if not day_df.empty:
     st.plotly_chart(fig, use_container_width=True)
 
 else:
-
     st.info("No data available for the selected date.")
